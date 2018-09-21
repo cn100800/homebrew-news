b02e153f9c00f9b95134d94568f325fc75908266
cn <cn100800@163.com>
2018-09-21 22:05:33 +0800

    news
    Signed-off-by: cn <cn100800@163.com>

--- php/app/index.php
+++ php/app/index.php
@@ -6,14 +6,18 @@ use app\client\client;
 use app\mail\mail;
 
 require __DIR__ . '/../vendor/autoload.php';
-
-
+$log_path = __DIR__ . '/../log/' . date('Y-m-d_H') . '.log';
 $client = new client();
 $params = config('home.home.params');
 $params['ot'] = time().'000';
 $data = $client->get(config('home.home.uri') , config('home.home.path'), $params);
 $body = "<html><head><title></title></head><body>";
-foreach ($data['Result'] as $key => $value) {
+foreach ($data['Result'] as $value) {
+    if (!empty($value['NewsTips'])) {
+        $value['NewsTips'][0]['TipName'] = '广告';
+        error_log(json_encode($value, JSON_UNESCAPED_UNICODE), 3, $log_path);
+        continue;
+    }
     $body .= "<h1>${value['title']}</h1><h2>${value['description']}</h2><br />";
 }
 $body .= "</body></html>";
